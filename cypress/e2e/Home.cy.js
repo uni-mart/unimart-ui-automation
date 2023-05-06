9b0d11756c53c7440801bfcde9fb9bff7c6a5fe7
Ashish Basnet <ashishbasnet468@gmail.com>
2023-05-06 22:13:27 +0100

    sprint-2

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test Home Page', () => {
+
+  it('should display home page', () => {
+    cy.visit('http://127.0.0.1:5173/');
+
+    cy.get('.d-inline-block', { timeout: 10000 }).should('be.visible');
+    cy.get('.bg-white', {timeout: 10000}).should('be.visible');
+    cy.get('[href="/login"]', {timeout: 10000}).should('be.visible');
+    cy.get('[href="/login"]').should('not.be.disabled');
+    cy.get('[href="/login"]').click();
   })
-})
+});
---
+++ cypress/e2e/MyProducts.cy.js
@@ -0,0 +1,16 @@
+describe('Test Product Page', () => {
+
+    it('should display product page', () => {
+      cy.visit('http://127.0.0.1:5173/myProducts');
+  
+      cy.get('.card-img-top')
+      .should('be.visible')
+      .invoke('attr', 'src')
+      .should('match', /^https?:\/\/.+$/)
+
+      cy.get('.card-body', { timeout: 10000 }).should('be.visible');
+      cy.get('.list-group', {timeout: 10000}).should('be.visible');
+      cy.get("input[placeholder='Search']").should('be.visible');
+  })
+      
+  });
