57507049c0fa7071a8c46580036ac8ca40c0ee40
bitcode001 <anil.rai00101@gmail.com>
2023-05-12 12:50:36 +0100

    fix(): updated path of UI for testing in production

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,11 +1,11 @@
 describe('Test Home Page', () => {
 
   it('should display home page', () => {
-    cy.visit('http://127.0.0.1:5173/');
+    cy.visit('http://localhost:4173/');
 
     cy.get('.d-inline-block', { timeout: 10000 }).should('be.visible');
-    cy.get('.bg-white', {timeout: 10000}).should('be.visible');
-    cy.get('[href="/login"]', {timeout: 10000}).should('be.visible');
+    cy.get('.bg-white', { timeout: 10000 }).should('be.visible');
+    cy.get('[href="/login"]', { timeout: 10000 }).should('be.visible');
     cy.get('[href="/login"]').should('not.be.disabled');
     cy.get('[href="/login"]').click();
     cy.screenshot('Home');
--- cypress/e2e/MyProducts.cy.js
+++ cypress/e2e/MyProducts.cy.js
@@ -1,17 +1,17 @@
 describe('Test Product Page', () => {
 
-    it('should display product page', () => {
-      cy.visit('http://127.0.0.1:5173/myProducts');
-  
-      cy.get('.card-img-top')
+  it('should display product page', () => {
+    cy.visit('http://localhost:4173/myProducts');
+
+    cy.get('.card-img-top')
       .should('be.visible')
       .invoke('attr', 'src')
       .should('match', /^https?:\/\/.+$/)
 
-      cy.get('.card-body', { timeout: 10000 }).should('be.visible');
-      cy.get('.list-group', {timeout: 10000}).should('be.visible');
-      cy.get("input[placeholder='Search']").should('be.visible');
-      cy.screenshot('Product');
+    cy.get('.card-body', { timeout: 10000 }).should('be.visible');
+    cy.get('.list-group', { timeout: 10000 }).should('be.visible');
+    cy.get("input[placeholder='Search']").should('be.visible');
+    cy.screenshot('Product');
   })
-      
-  });
+
+});
